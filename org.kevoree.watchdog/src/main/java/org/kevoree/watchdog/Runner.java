3c9d19f52b5b0c1242a05d7ca4a1d3b77bf1a314
Grégory NAIN <gregory.nain@gmail.com>
2013-11-18 20:21:55 +0100

    Gong to MavenResolver

--- org.kevoree.watchdog/src/main/java/org/kevoree/watchdog/Runner.java
+++ org.kevoree.watchdog/src/main/java/org/kevoree/watchdog/Runner.java
@@ -1,8 +1,11 @@
 package org.kevoree.watchdog;
 
+import org.kevoree.resolver.MavenResolver;
+
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 
 /**
  * Created by duke on 16/05/13.
@@ -14,10 +17,17 @@ public class Runner {
     public static final String outFileLogProperty = "log.out";
     public static final String errFileLogProperty = "log.err";
 
+
+    private static final String baseRuntimeMvnUrl = "mvn:org.kevoree.platform:org.kevoree.platform.standalone:";
+    private static final String mavenCentral = "http://repo.maven.apache.org/maven2";
+    private static final String ossRepository = "http://oss.sonatype.org/content/groups/public";
+    private static MavenResolver artifactResolver = new MavenResolver();
+
+
     private static final String runtimeBaseURL = "http://oss.sonatype.org/content/groups/public/org/kevoree/platform/org.kevoree.platform.standalone/";
     private static final String runtimeURL = runtimeBaseURL + "kevoreeVersion/org.kevoree.platform.standalone-kevoreeVersion.jar";
     private static RuntimeDowloader downloader = new RuntimeDowloader();
-    private static MavenVersionResolver snapshotResolver = new MavenVersionResolver();
+    //private static MavenVersionResolver snapshotResolver = new MavenVersionResolver();
     private static ChildManager childManager = new ChildManager();
     public static WatchDogCheck checker = new WatchDogCheck();
 
@@ -57,71 +67,54 @@ public class Runner {
         String kevoreeVersion = "LATEST";
         //String kevoreeVersion = "RELEASE";
 
-
         if (args.length > 0) {
             kevoreeVersion = args[0];
         }
 
-        if (kevoreeVersion.equals("LATEST")) {
-            kevoreeVersion = snapshotResolver.getLastVersion(runtimeBaseURL, true, false);
-            System.out.println("LATEST -> " + kevoreeVersion);
-            if (kevoreeVersion == null) {
-                System.exit(-1);
-            }
-        }
-        if (kevoreeVersion.equals("RELEASE")) {
-            String cleanRuntimeURL = runtimeBaseURL.replaceAll("kevoreeVersion", kevoreeVersion);
-            kevoreeVersion = snapshotResolver.getLastVersion(cleanRuntimeURL, false, true);
-            System.out.println("RELEASE -> " + kevoreeVersion);
-            if (kevoreeVersion == null) {
-                System.exit(-1);
-            }
-        }
 
-        String kevoreeVersionBackup = kevoreeVersion;
+        String runtimeMvnUrl = baseRuntimeMvnUrl + kevoreeVersion;
+        ArrayList<String> repos = new ArrayList<String>();
+        repos.add(mavenCentral);
+        repos.add(ossRepository);
+        File runtime = artifactResolver.resolve(runtimeMvnUrl, repos);
 
+        if(runtime != null) {
+            checker.setRuntimeFile(runtime);
+            childManager.setSubProcess(checker);
 
-        String cleanRuntimeURL = runtimeURL.replaceAll("kevoreeVersion", kevoreeVersion);
-        if (kevoreeVersion.endsWith("SNAPSHOT")) {
-            kevoreeVersion = kevoreeVersion.replace("SNAPSHOT", snapshotResolver.getLastVersion(cleanRuntimeURL, false, false));
-            //CLEANUP URL WITH MAVEN RESOLVED VERSION
-            cleanRuntimeURL = runtimeURL.replaceFirst("kevoreeVersion", kevoreeVersionBackup);
-            cleanRuntimeURL = cleanRuntimeURL.replaceAll("kevoreeVersion", kevoreeVersion);
-        }
-        File runtime = downloader.get(cleanRuntimeURL, kevoreeVersion);
-        checker.setRuntimeFile(runtime);
-        childManager.setSubProcess(checker);
-
-        Object fileoutNameProp = System.getProperty(outFileLogProperty);
-        if (fileoutNameProp != null) {
-            File syso = new File(fileoutNameProp.toString());
-            if (syso.exists()) {
-                syso.renameTo(new File(fileoutNameProp.toString()+"-"+System.currentTimeMillis()));
-                syso = new File(fileoutNameProp.toString());
+            Object fileoutNameProp = System.getProperty(outFileLogProperty);
+            if (fileoutNameProp != null) {
+                File syso = new File(fileoutNameProp.toString());
+                if (syso.exists()) {
+                    syso.renameTo(new File(fileoutNameProp.toString()+"-"+System.currentTimeMillis()));
+                    syso = new File(fileoutNameProp.toString());
+                }
+                checker.setSysoutFile(syso);
             }
-            checker.setSysoutFile(syso);
-        }
-        Object fileerrNameProp = System.getProperty(errFileLogProperty);
-        if (fileerrNameProp != null) {
-            File syserr = new File(fileerrNameProp.toString());
-            if (syserr.exists()) {
-                syserr.renameTo(new File(fileerrNameProp.toString()+"-"+System.currentTimeMillis()));
-                syserr = new File(fileerrNameProp.toString());
+            Object fileerrNameProp = System.getProperty(errFileLogProperty);
+            if (fileerrNameProp != null) {
+                File syserr = new File(fileerrNameProp.toString());
+                if (syserr.exists()) {
+                    syserr.renameTo(new File(fileerrNameProp.toString()+"-"+System.currentTimeMillis()));
+                    syserr = new File(fileerrNameProp.toString());
+                }
+                checker.setSyserrFile(syserr);
             }
-            checker.setSyserrFile(syserr);
-        }
 
-        //look for bootmodel
-        if (args.length == 2) {
-            String modelPath = args[1];
-            File modelFile = new File(modelPath);
-            if (modelFile.exists()) {
-                checker.setModelFile(modelFile);
+            //look for bootmodel
+            if (args.length == 2) {
+                String modelPath = args[1];
+                File modelFile = new File(modelPath);
+                if (modelFile.exists()) {
+                    checker.setModelFile(modelFile);
+                }
             }
+            System.out.println("Kevoree " + kevoreeVersion + " Ready to run ");
+            checker.startServer();
+            checker.startKevoreeProcess();
+        } else {
+            System.err.println("Could not start runtime, because the file returned by the MavenResolver for " + runtimeMvnUrl + " is null");
         }
-        System.out.println("Kevoree " + kevoreeVersion + " Ready to run ");
-        checker.startServer();
-        checker.startKevoreeProcess();
     }
 
 }
--- org.kevoree.watchdog/src/main/java/org/kevoree/watchdog/RuntimeDowloader.java
+++ org.kevoree.watchdog/src/main/java/org/kevoree/watchdog/RuntimeDowloader.java
@@ -1,5 +1,7 @@
 package org.kevoree.watchdog;
 
+import org.kevoree.resolver.MavenResolver;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -7,6 +9,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
 import java.util.jar.JarOutputStream;
 import java.util.zip.ZipEntry;
 
@@ -15,45 +18,14 @@ import java.util.zip.ZipEntry;
  */
 public class RuntimeDowloader {
 
+
+
     private String getTempPath() {
         String tempPath = System.getProperty("java.io.tmpdir");
         if (!tempPath.endsWith(File.separator)) {
             tempPath = tempPath + File.separator;
         }
         return tempPath;
-    }
-
-    public File get(String runtimeURL, String version) throws IOException {
-        File runtimeFile = getCache(runtimeURL, version);
-        if (!runtimeFile.exists()) {
-            URL url = new URL(runtimeURL);
-            System.out.println("Runtime not found, downloading it...");
-
-            if (System.getProperty("java.vm.vendor").toLowerCase().contains("robovm")) {
-                InputStream in = url.openStream();
-                FileOutputStream fos = new FileOutputStream(runtimeFile);
-                byte data[] = new byte[1024];
-                int count;
-                while ((count = in.read(data, 0, 1024)) != -1) {
-                    fos.write(data, 0, count);
-                }
-                in.close();
-                fos.close();
-            } else {
-                ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-                FileOutputStream fos = new FileOutputStream(runtimeFile);
-                fos.getChannel().transferFrom(rbc, 0, 1 << 24);
-                fos.close();
-            }
-        }
-        return runtimeFile;
-    }
-
-    public File getCache(String runtimeURL, String version) {
-        String tempPath = getTempPath();
-        File runtimeFile = new File(tempPath + File.separator + "kevoree-runtime-" + version + ".jar");
-        System.out.println(runtimeFile);
-        return runtimeFile;
     }
 
     public File getSharedChildClassJar() {
