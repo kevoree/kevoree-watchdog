adddc3fd95cbd3efea6c85d491e211b5af5a0d1e
Francois Fouquet <fouquet.f@gmail.com>
2013-12-04 11:15:13 +0100

    Watchdog version 0.18

--- org.kevoree.watchdog/src/main/java/org/kevoree/watchdog/Version.java
+++ org.kevoree.watchdog/src/main/java/org/kevoree/watchdog/Version.java
@@ -1,6 +1,6 @@
 package org.kevoree.watchdog;
 public final class Version {
- public static String VERSION="0.18-SNAPSHOT";
+ public static String VERSION="0.18";
  public static String GROUP="org.kevoree.watchdog";
  public static String NAME="org.kevoree.watchdog";
 }
