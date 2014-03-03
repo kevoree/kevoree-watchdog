4e180fd7653bc803d169f2a2ac8f7b140c6bf186
Francois Fouquet <fouquet.f@gmail.com>
2014-03-03 11:32:22 +0100

    next version

--- org.kevoree.watchdog/src/main/java/org/kevoree/watchdog/Version.java
+++ org.kevoree.watchdog/src/main/java/org/kevoree/watchdog/Version.java
@@ -1,6 +1,6 @@
 package org.kevoree.watchdog;
 public final class Version {
- public static String VERSION="0.21-SNAPSHOT";
+ public static String VERSION="0.21";
  public static String GROUP="org.kevoree.watchdog";
  public static String NAME="org.kevoree.watchdog";
 }
