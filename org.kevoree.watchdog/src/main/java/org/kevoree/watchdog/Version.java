9442f3249b262429916a9380cea3a559aa526589
Francois Fouquet <fouquet.f@gmail.com>
2014-05-27 22:38:38 +0200

    release

--- org.kevoree.watchdog/src/main/java/org/kevoree/watchdog/Version.java
+++ org.kevoree.watchdog/src/main/java/org/kevoree/watchdog/Version.java
@@ -1,6 +1,6 @@
 package org.kevoree.watchdog;
 public final class Version {
- public static String VERSION="0.22";
+ public static String VERSION="0.23";
  public static String GROUP="org.kevoree.watchdog";
  public static String NAME="org.kevoree.watchdog";
 }
